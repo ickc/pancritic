67e20b6be1a3b3e794e91e5b1682833d6ad8c1e2
KolenCheung <christian.kolen@gmail.com>
2016-05-08 03:31:31 -0700

    added test file

---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+# Preparation: I made a copy first so that your test is not overwritten
+cp test.md test-accept.md
+cp test.md test-reject.md
+
+# Some Examples
+## Showing Difference and not overwriting
+./pandoc-criticmarkup.sh -d html test.md | pandoc -s -c pandoc-criticmarkup.css -o test.html
+./pandoc-criticmarkup.sh -d latex test.md | pandoc -s -o test.tex
+./pandoc-criticmarkup.sh -d pdf test.md | pandoc -s -o test.pdf
+## accept or reject while overwriting the source
+### accept
+./pandoc-criticmarkup.sh -ap test-accept.md
+### reject
+./pandoc-criticmarkup.sh -rp test-reject.md
+## accept or reject, but keep the CriticMarkup in the source for future reference
+### accept
+./pandoc-criticmarkup.sh -a test.md | pandoc -s -c pandoc-criticmarkup.css -o test-accept.html
+./pandoc-criticmarkup.sh -a test.md | pandoc -s -o test-accept.tex
+./pandoc-criticmarkup.sh -a test.md | pandoc -s -o test-accept.pdf
+### reject
+./pandoc-criticmarkup.sh -r test.md | pandoc -s -c pandoc-criticmarkup.css -o test-reject.html
+./pandoc-criticmarkup.sh -r test.md | pandoc -s -o test-reject.tex
+./pandoc-criticmarkup.sh -r test.md | pandoc -s -o test-reject.pdf
