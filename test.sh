0a241a9e17367588b45ade17a7c14953ed12e99b
KolenCheung <christian.kolen@gmail.com>
2016-09-30 14:32:59 -0700

    remove auto generated files

--- test.sh
+++ test.sh
@@ -1,26 +1,57 @@
 #!/bin/bash
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Preparation: I made a copy first so that your test is not overwritten
-cp test.md test-accept.md
-cp test.md test-reject.md
+# test/test folder
+mkdir -p test
 
-# Some Examples
-## Showing Difference and not overwriting
-./pandoc-criticmarkup.sh -d html test.md | pandoc -s -c pandoc-criticmarkup.css -o test.html
-./pandoc-criticmarkup.sh -d latex test.md | pandoc -s -o test.tex
-./pandoc-criticmarkup.sh -d pdf test.md | pandoc -s -o test.pdf
-## accept or reject while overwriting the source
-### accept
-./pandoc-criticmarkup.sh -ap test-accept.md
-### reject
-./pandoc-criticmarkup.sh -rp test-reject.md
+# README.md
+
+## Preparation: I made a copy first so that your README is not overwritten
+cp README.md test/README-accept.md
+cp README.md test/README-reject.md
+
+## Some Examples
+### Showing Difference and not overwriting
+pandoc-criticmarkup.sh -d html README.md | pandoc -s -c pandoc-criticmarkup.css -o test/README.html
+pandoc-criticmarkup.sh -d latex README.md | pandoc -s -o test/README.tex
+pandoc-criticmarkup.sh -d pdf README.md | pandoc -s -o test/README.pdf
+### accept or reject while overwriting the source
+#### accept
+pandoc-criticmarkup.sh -ap test/README-accept.md
+#### reject
+pandoc-criticmarkup.sh -rp test/README-reject.md
 ## accept or reject, but keep the CriticMarkup in the source for future reference
+#### accept
+pandoc-criticmarkup.sh -a README.md | pandoc -s -c pandoc-criticmarkup.css -o test/README-accept.html
+pandoc-criticmarkup.sh -a README.md | pandoc -s -o test/README-accept.tex
+pandoc-criticmarkup.sh -a README.md | pandoc -s -o test/README-accept.pdf
+#### reject
+pandoc-criticmarkup.sh -r README.md | pandoc -s -c pandoc-criticmarkup.css -o test/README-reject.html
+pandoc-criticmarkup.sh -r README.md | pandoc -s -o test/README-reject.tex
+pandoc-criticmarkup.sh -r README.md | pandoc -s -o test/README-reject.pdf
+
+# test.md
+
+## Preparation: I made a copy first so that your test is not overwritten
+cp test.md test/test-accept.md
+cp test.md test/test-reject.md
+
+## Some Examples
+### Showing Difference and not overwriting
+pandoc-criticmarkup.sh -d html test.md | pandoc -s -c pandoc-criticmarkup.css -o test/test.html
+pandoc-criticmarkup.sh -d latex test.md | pandoc -s -o test/test.tex
+pandoc-criticmarkup.sh -d pdf test.md | pandoc -s -o test/test.pdf
+### accept or reject while overwriting the source
 ### accept
-./pandoc-criticmarkup.sh -a test.md | pandoc -s -c pandoc-criticmarkup.css -o test-accept.html
-./pandoc-criticmarkup.sh -a test.md | pandoc -s -o test-accept.tex
-./pandoc-criticmarkup.sh -a test.md | pandoc -s -o test-accept.pdf
-### reject
-./pandoc-criticmarkup.sh -r test.md | pandoc -s -c pandoc-criticmarkup.css -o test-reject.html
-./pandoc-criticmarkup.sh -r test.md | pandoc -s -o test-reject.tex
-./pandoc-criticmarkup.sh -r test.md | pandoc -s -o test-reject.pdf
+pandoc-criticmarkup.sh -ap test/test-accept.md
+#### reject
+pandoc-criticmarkup.sh -rp test/test-reject.md
+## accept or reject, but keep the CriticMarkup in the source for future reference
+#### accept
+pandoc-criticmarkup.sh -a test.md | pandoc -s -c pandoc-criticmarkup.css -o test/test-accept.html
+pandoc-criticmarkup.sh -a test.md | pandoc -s -o test/test-accept.tex
+pandoc-criticmarkup.sh -a test.md | pandoc -s -o test/test-accept.pdf
+#### reject
+pandoc-criticmarkup.sh -r test.md | pandoc -s -c pandoc-criticmarkup.css -o test/test-reject.html
+pandoc-criticmarkup.sh -r test.md | pandoc -s -o test/test-reject.tex
+pandoc-criticmarkup.sh -r test.md | pandoc -s -o test/test-reject.pdf
