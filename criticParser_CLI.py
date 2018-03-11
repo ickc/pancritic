e7dcc809fcab027058927da204a1a26b60e4f8ab
Kolen <christian.kolen@gmail.com>
2018-03-10 22:29:43 -0700

    criticParser_CLI.py: add tex output support; add --critic-mode

--- criticParser_CLI.py
+++ criticParser_CLI.py
@@ -1,25 +1,10 @@
 #!/usr/bin/env python
 
-'''
-Input is quoted UNIX type file path.
-
-Output is HTML formated text using ins, del, and aside tags
-
-File is written to the same directory as the source unless specified with the -o flag
-
--m2     Uses the markdown2 module
-
--o <file_path>    Writes file to specified path. Must include file name
-
--b Opens the output HTML file in the defualt browser
-'''
-
 from __future__ import print_function
 
 import argparse
 import os
 import re
-import subprocess
 import sys
 
 CSS = '''<style>
@@ -217,9 +202,43 @@ BODY_BEGIN = '''</head><body><div id="wrapper">'''
 
 BODY_END = '''</div></body></html>'''
 
+
+ADD_EDIT = re.compile(r'(?s)\{\+\+(.*?)\+\+[ \t]*(\[(.*?)\])?[ \t]*\}')
+DEL_EDIT = re.compile(r'(?s)\{\-\-(.*?)\-\-[ \t]*(\[(.*?)\])?[ \t]*\}')
+COMM_EDIT = re.compile(r'(?s)\{\>\>(.*?)\<\<[ \t]*(\[(.*?)\])?[ \t]*\}')
+MARK_EDIT = re.compile(r'(?s)\{\=\=(.*?)\=\=[ \t]*(\[(.*?)\])?[ \t]*\}')
+SUB_EDIT = re.compile(r'''(?s)\{\~\~(?P<original>(?:[^\~\>]|(?:\~(?!\>)))+)\~\>(?P<new>(?:[^\~\~]|(?:\~(?!\~\})))+)\~\~\}''')
+
 # filters
 
-def criticmarkup_filter(body):
+def criticmarkup_accept_filter(body):
+    body = ADD_EDIT.sub(r'\1', body)
+    body = DEL_EDIT.sub(r'', body)
+    body = SUB_EDIT.sub(r'\2', body)
+
+    body = MARK_EDIT.sub(r'\1', body)
+    return COMM_EDIT.sub(r'', body)
+
+
+def criticmarkup_reject_filter(body):
+    body = ADD_EDIT.sub(r'', body)
+    body = DEL_EDIT.sub(r'\1', body)
+    body = SUB_EDIT.sub(r'\1', body)
+
+    body = MARK_EDIT.sub(r'\1', body)
+    return COMM_EDIT.sub(r'', body)
+
+
+def criticmarkup_tex_diff_filter(body):
+    body = ADD_EDIT.sub(r'\\underline{\1}', body)
+    body = DEL_EDIT.sub(r'\\st{\1}', body)
+    body = SUB_EDIT.sub(r'\\st{\1}\\underline{\2}', body)
+
+    body = MARK_EDIT.sub(r'\\hl{\1}', body)
+    return COMM_EDIT.sub(r'\\marginpar{\1}', body)
+
+
+def criticmarkup_html_diff_filter(body):
 
     def deletionProcess(group_object):
         replaceString = ''
@@ -298,8 +317,6 @@ def markdown_filter(body, engine):
     def fallback():
         print('Cannot use {}, use markdown instead.'.format(engine), file=sys.stderr)
 
-    print('Using the {} module for processing'.format(engine))
-
     if engine == 'markdown2':
         try:
             from markdown2 import markdown
@@ -328,22 +345,75 @@ def markdown_filter(body, engine):
     return markdown(body, extensions=['extra', 'codehilite', 'meta'])
 
 
+def tex_filter(body, engine, standalone):
+    extra_args = ['-s'] if standalone else None
+
+    if engine == 'panflute':
+        try:
+            from panflute import convert_text
+            return convert_text(body, output_format='latex', extra_args=extra_args)
+        except:
+            print('Cannot use {}, use pypandoc instead.'.format(engine), file=sys.stderr)
+
+    try:
+        from pypandoc import convert_text
+        return convert_text(body, 'latex', format='markdown', extra_args=extra_args)
+    except:
+        print('Cannot use {}, stop converting to tex and output markdown instead.'.format(engine), file=sys.stderr)
+
+    return body
+
+
 def html_filter(body, css, standalone=False):
     if standalone:
         return HEAD + css + BODY_BEGIN + body + BODY_END
     else:
         return css + '<div id="wrapper">\n\n' + body + '\n\n</div>\n'
 
+# helper
+
+def output_to_format(output):
+    ext = os.path.splitext(output.name)[1][1:]
+    if ext == 'md':
+        return 'markdown'
+    elif ext == 'tex':
+        return 'latex'
+    else:
+        return ext
+
 
 def main(args):
     body = args.input.read()
 
-    body = criticmarkup_filter(body)
+    # diff mode
+    if args.critic_mode[0] == 'd':
+        if args.to in ('markdown', 'html'):
+            body = criticmarkup_html_diff_filter(body)
+        elif args.to == 'latex':
+            body = criticmarkup_tex_diff_filter(body)
+    # accept mode
+    elif args.critic_mode[0] == 'a':
+        body = criticmarkup_accept_filter(body)
+    # reject mode
+    elif args.critic_mode[0] == 'r':
+        body = criticmarkup_reject_filter(body)
+    else:
+        print('Unknown critic mode {}.'.format(args.critic_mode), file=sys.stderr)
 
-    if args.to == 'html':
-        body = markdown_filter(body, args.html_engine)
+    # only convert markdown to html or tex if the output extension is really that format
+    if output_to_format(args.output) == 'html':
+        body = markdown_filter(body, args.engine)
+    elif output_to_format(args.output) == 'latex':
+        body = tex_filter(body, args.engine, args.standalone)
 
-    body = html_filter(body, (css_file.read() if args.css else CSS), standalone=args.standalone)
+    if args.to in ('markdown', 'html'):
+        if args.css:
+            css = css_file.read()
+        elif args.critic_mode[0] == 'a' or args.critic_mode[0] == 'r':
+            css = ''
+        else:
+            css = CSS
+        body = html_filter(body, css, standalone=args.standalone)
 
     args.output.write(body)
 
@@ -352,23 +422,26 @@ def cli():
     parser = argparse.ArgumentParser(description='Convert Critic Markup.')
 
     parser.add_argument('input', type=argparse.FileType('r'), default=sys.stdin,
-                        help='Input file. Default: stdin')
+                        help='Input file. Default: stdin.')
     parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=sys.stdout,
-                        help='Output file. Default: sys.stdout')
+                        help='Output file. Default: stdout.')
     parser.add_argument('-c', '--css', type=argparse.FileType('r'),
                         help='Custom CSS file. If not specified, default CSS is used, where minimal javascript is embedded.')
 
     parser.add_argument('-t', '--to',
-                        help='Output format. Default: inferred from --output. Valid: md, html.')
+                        help='Output format. Default: inferred from --output. Valid: markdown, html, latex.')
     parser.add_argument('-s', '--standalone', action='store_true',
                         help='Output standalone html, only useful when output to html.')
-    parser.add_argument('--html-engine', default='markdown',
+    parser.add_argument('--engine', default='markdown',
                         help='If specified, convert markdown to HTML using the specified engine. Default: markdown. Valid: markdown, markdown2, panflute, pypandoc.')
+
+    parser.add_argument('-m', '--critic-mode', default='diff',
+                        help='Specify critic mode. Default: diff. Valid: a/accept, r/reject, d/diff.')
 
     args = parser.parse_args()
 
     if not args.to:
-        args.to = os.path.splitext(args.output.name)[1][1:]
+        args.to = output_to_format(args.output)
 
     return args
 
