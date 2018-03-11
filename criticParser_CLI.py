8e97ffa3ba76c6cb1c90e180fe2e5eb302f7387e
Kolen <christian.kolen@gmail.com>
2018-03-10 17:19:24 -0800

    criticParser_CLI.py: refactor and cleanup

--- criticParser_CLI.py
+++ criticParser_CLI.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
 
+'''
+Input is quoted UNIX type file path.
+
+Output is HTML formated text using ins, del, and aside tags
+
+File is written to the same directory as the source unless specified with the -o flag
+
+-m2     Uses the markdown2 module
+
+-o <file_path>    Writes file to specified path. Must include file name
+
+-b Opens the output HTML file in the defualt browser
+'''
+
 from __future__ import print_function
-import codecs
+
 import sys
 import os
 import re
 import argparse
 import subprocess
-
-# Input is quoted UNIX type file path.
-
-# Output is HTML formated text using ins, del, and aside tags
-
-# File is written to the same directory as the source unless specified with the -o flag
-
-# -m2     Uses the markdown2 module
-
-# -o <file_path>    Writes file to specified path. Must include file name
-#
-# -b Opens the output HTML file in the defualt browser
-
 
 add_pattern = r'''(?s)\{\+\+(?P<value>.*?)\+\+[ \t]*(\[(?P<meta>.*?)\])?[ \t]*\}'''
 
@@ -32,62 +33,6 @@ gen_comm_pattern = r'''(?s)\{[ \t]*\[(?P<meta>.*?)\][ \t]*\}'''
 subs_pattern = r'''(?s)\{\~\~(?P<original>(?:[^\~\>]|(?:\~(?!\>)))+)\~\>(?P<new>(?:[^\~\~]|(?:\~(?!\~\})))+)\~\~\}'''
 
 mark_pattern = r'''(?s)\{\=\=(?P<value>.*?)\=\=\}\{\>\>(?P<comment>.*?)\<\<\}'''
-
-
-# Considered for future standalone highlight without comment
-# mark_pattern = r'''(?s)\{\{(?P<value>.*?)\}\}'''
-
-
-def deletionProcess(group_object):
-    replaceString = ''
-    if group_object.group('value') == '\n\n':
-        replaceString = "<del>&nbsp;</del>"
-    else:
-        replaceString = '<del>' + group_object.group('value').replace("\n\n", "&nbsp;") + '</del>'
-    return replaceString
-
-
-def subsProcess(group_object):
-    delString = '<del>' + group_object.group('original') + '</del>'
-    insString = '<ins>' + group_object.group('new') + '</ins>'
-    newString = delString + insString
-    return newString
-
-
-# Converts Addition markup to HTML
-def additionProcess(group_object):
-    replaceString = ''
-
-    # Is there a new paragraph followed by new text
-    if group_object.group('value').startswith('\n\n') and group_object.group('value') != "\n\n":
-        replaceString = "\n\n<ins class='critic' break>&nbsp;</ins>\n\n"
-        replaceString = replaceString + '<ins>' + group_object.group('value').replace("\n", " ")
-        replaceString = replaceString + '</ins>'
-
-    # Is the addition just a single new paragraph
-    elif group_object.group('value') == "\n\n":
-        replaceString = "\n\n<ins class='critic break'>&nbsp;" + '</ins>\n\n'
-
-    # Is it added text followed by a new paragraph?
-    elif group_object.group('value').endswith('\n\n') and group_object.group('value') != "\n\n":
-        replaceString = '<ins>' + group_object.group('value').replace("\n", " ") + '</ins>'
-        replaceString = replaceString + "\n\n<ins class='critic break'>&nbsp;</ins>\n\n"
-
-    else:
-        replaceString = '<ins>' + group_object.group('value').replace("\n", " ") + '</ins>'
-
-    return replaceString
-
-
-def highlightProcess(group_object):
-    replaceString = '<span class="critic comment">' + group_object.group('value').replace("\n", " ") + '</span>'
-    return replaceString
-
-
-def markProcess(group_object):
-    replaceString = '<mark>' + group_object.group('value') + '</mark><span class="critic comment">' + group_object.group('comment').replace("\n", " ") + '</span>'
-    return replaceString
-
 
 a = '''
 
@@ -296,88 +241,143 @@ bodybegin = '''</head><body><div id="wrapper">'''
 headend = '''</div></body></html>'''
 
 
-# Accept input from Marked.app
-
-# h = sys.stdin.read()
-
-
-parser = argparse.ArgumentParser(description='Convert Critic Markup to HTML')
-parser.add_argument('source', help='The source file path, including file name')
-parser.add_argument('-m2', help='Use the markdown2 python module. If left blank then markdown module is used', action='store_true')
-parser.add_argument('-o', '--output', help='Path to store the output file, including file name', metavar='out-file', type=argparse.FileType('wt'), required=False)
-parser.add_argument('-css', '--css', help='Path to a custom CSS file, including file name', metavar='in-file', type=argparse.FileType('rt'), required=False)
-parser.add_argument('-b', '--browser', help='View the output file in the default browser after saving.', action='store_true')
-
-args = parser.parse_args()
-try:
-
-    if args.source:
-        inputFile = open(args.source, "r")
-        inputText = inputFile.read()
-        inputFile.close()
+def deletionProcess(group_object):
+    replaceString = ''
+    if group_object.group('value') == '\n\n':
+        replaceString = "<del>&nbsp;</del>"
     else:
-        log("No source file specified")
-        print("No source file specified")
-        sys.exit(1)
+        replaceString = '<del>' + group_object.group('value').replace("\n\n", "&nbsp;") + '</del>'
+    return replaceString
 
-    h = inputText
 
-    h = re.sub(del_pattern, deletionProcess, inputText, flags=re.DOTALL)
+def subsProcess(group_object):
+    delString = '<del>' + group_object.group('original') + '</del>'
+    insString = '<ins>' + group_object.group('new') + '</ins>'
+    newString = delString + insString
+    return newString
 
-    h = re.sub(add_pattern, additionProcess, h, flags=re.DOTALL)
 
-    h = re.sub(mark_pattern, markProcess, h, flags=re.DOTALL)
+# Converts Addition markup to HTML
+def additionProcess(group_object):
+    replaceString = ''
 
-    # comment processing must come after highlights
-    h = re.sub(comm_pattern, highlightProcess, h, flags=re.DOTALL)
+    # Is there a new paragraph followed by new text
+    if group_object.group('value').startswith('\n\n') and group_object.group('value') != "\n\n":
+        replaceString = "\n\n<ins class='critic' break>&nbsp;</ins>\n\n"
+        replaceString = replaceString + '<ins>' + group_object.group('value').replace("\n", " ")
+        replaceString = replaceString + '</ins>'
 
-    h = re.sub(subs_pattern, subsProcess, h, flags=re.DOTALL)
+    # Is the addition just a single new paragraph
+    elif group_object.group('value') == "\n\n":
+        replaceString = "\n\n<ins class='critic break'>&nbsp;" + '</ins>\n\n'
 
-    if (args.m2):
-        import markdown2
-        h = markdown2.markdown(h, extras=['footnotes', 'fenced-code-blocks', 'cuddled-lists', 'code-friendly'])
-        print('\nUsing the Markdown2 module for processing')
+    # Is it added text followed by a new paragraph?
+    elif group_object.group('value').endswith('\n\n') and group_object.group('value') != "\n\n":
+        replaceString = '<ins>' + group_object.group('value').replace("\n", " ") + '</ins>'
+        replaceString = replaceString + "\n\n<ins class='critic break'>&nbsp;</ins>\n\n"
+
     else:
-        import markdown
-        h = markdown.markdown(h, extensions=['extra', 'codehilite', 'meta'])
+        replaceString = '<ins>' + group_object.group('value').replace("\n", " ") + '</ins>'
 
-    if (args.css):
-        css_file = args.css
-        cssText = css_file.read()
-        css_file.close()
-        h = head + cssText + bodybegin + h + headend
-    else:
-        h = jq + a + bodybegin + h + headend
+    return replaceString
 
-    # If an output file is specified, write to it
-    if args.output:
-        filesource = args.output
-        abs_path = os.path.abspath(filesource.name)
-        output_file = abs_path
-        print(output_file)
-        #file = open(filename, 'wb')
-        filesource.write(h)
-        filesource.close()
-        print("\nOutput file created:  ", abs_path)
-    else:
-        path, filename = os.path.split(args.source)
-        print("Converting >> " + args.source)
-        output_file = path + '/' + filename.split(os.extsep, 1)[0] + '_CriticParseOut.html'
-        file = open(output_file, 'w')
-        file.write(h.encode('utf-8'))
-        file.close()
-        print("\nOutput file created:  " + output_file)
 
-    if (args.browser):
-        try:
-            retcode = subprocess.call("open " + output_file, shell=True)
-            if retcode < 0:
-                print("Child was terminated by signal", -retcode, file=sys.stderr)
-            else:
-                print("Child returned", retcode, file=sys.stderr)
-        except OSError as e:
-            print("Execution failed:", e, file=sys.stderr)
+def highlightProcess(group_object):
+    replaceString = '<span class="critic comment">' + group_object.group('value').replace("\n", " ") + '</span>'
+    return replaceString
 
-except:
-    print("Unexpected Error: ", sys.exc_info()[0])
-    raise
+
+def markProcess(group_object):
+    replaceString = '<mark>' + group_object.group('value') + '</mark><span class="critic comment">' + group_object.group('comment').replace("\n", " ") + '</span>'
+    return replaceString
+
+
+def main(args):
+    try:
+
+        if args.source:
+            inputFile = open(args.source, "r")
+            inputText = inputFile.read()
+            inputFile.close()
+        else:
+            log("No source file specified")
+            print("No source file specified")
+            sys.exit(1)
+
+        h = inputText
+
+        h = re.sub(del_pattern, deletionProcess, inputText, flags=re.DOTALL)
+
+        h = re.sub(add_pattern, additionProcess, h, flags=re.DOTALL)
+
+        h = re.sub(mark_pattern, markProcess, h, flags=re.DOTALL)
+
+        # comment processing must come after highlights
+        h = re.sub(comm_pattern, highlightProcess, h, flags=re.DOTALL)
+
+        h = re.sub(subs_pattern, subsProcess, h, flags=re.DOTALL)
+
+        if (args.m2):
+            import markdown2
+            h = markdown2.markdown(h, extras=['footnotes', 'fenced-code-blocks', 'cuddled-lists', 'code-friendly'])
+            print('\nUsing the Markdown2 module for processing')
+        else:
+            import markdown
+            h = markdown.markdown(h, extensions=['extra', 'codehilite', 'meta'])
+
+        if (args.css):
+            css_file = args.css
+            cssText = css_file.read()
+            css_file.close()
+            h = head + cssText + bodybegin + h + headend
+        else:
+            h = jq + a + bodybegin + h + headend
+
+        # If an output file is specified, write to it
+        if args.output:
+            filesource = args.output
+            abs_path = os.path.abspath(filesource.name)
+            output_file = abs_path
+            print(output_file)
+            #file = open(filename, 'wb')
+            filesource.write(h)
+            filesource.close()
+            print("\nOutput file created:  ", abs_path)
+        else:
+            path, filename = os.path.split(args.source)
+            print("Converting >> " + args.source)
+            output_file = path + '/' + filename.split(os.extsep, 1)[0] + '_CriticParseOut.html'
+            file = open(output_file, 'w')
+            file.write(h.encode('utf-8'))
+            file.close()
+            print("\nOutput file created:  " + output_file)
+
+        if (args.browser):
+            try:
+                retcode = subprocess.call("open " + output_file, shell=True)
+                if retcode < 0:
+                    print("Child was terminated by signal", -retcode, file=sys.stderr)
+                else:
+                    print("Child returned", retcode, file=sys.stderr)
+            except OSError as e:
+                print("Execution failed:", e, file=sys.stderr)
+
+    except:
+        print("Unexpected Error: ", sys.exc_info()[0])
+        raise
+
+
+def cli():
+
+    parser = argparse.ArgumentParser(description='Convert Critic Markup to HTML')
+    parser.add_argument('source', help='The source file path, including file name')
+    parser.add_argument('-m2', help='Use the markdown2 python module. If left blank then markdown module is used', action='store_true')
+    parser.add_argument('-o', '--output', help='Path to store the output file, including file name', metavar='out-file', type=argparse.FileType('wt'), required=False)
+    parser.add_argument('-css', '--css', help='Path to a custom CSS file, including file name', metavar='in-file', type=argparse.FileType('rt'), required=False)
+    parser.add_argument('-b', '--browser', help='View the output file in the default browser after saving.', action='store_true')
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main(cli())
