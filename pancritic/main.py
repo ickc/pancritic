2a2318bc992af80ae00b958de18679ef97aa0fd8
Kolen <christian.kolen@gmail.com>
2018-03-11 21:13:28 -0700

    pancritic: improve IO handling and add `-i`

--- pancritic/main.py
+++ pancritic/main.py
@@ -9,8 +9,7 @@ from .filters import criticmarkup_accept_filter, criticmarkup_reject_filter, cri
 from .version import __version__
 
 
-def IO_to_format(output):
-    ext = os.path.splitext(output.name)[1][1:]
+def normalize_format(ext):
     if ext == 'md':
         return 'markdown'
     elif ext == 'tex':
@@ -19,9 +18,8 @@ def IO_to_format(output):
         return ext
 
 
-def main(args):
-    body = args.input.read()
-
+def main(args, body, output_format, is_binary):
+    # parse CritiMarkup
     # diff/markup mode
     if args.critic_mode[0] in ('d', 'm'):
         if args.to == 'latex':
@@ -38,29 +36,35 @@ def main(args):
     else:
         print('Unknown critic mode {}.'.format(args.critic_mode), file=sys.stderr)
 
+    # convert between to and from format
     # only convert markdown to html or tex if the output extension is really that format
-    output_format = IO_to_format(args.output)
     if args.from_format == 'markdown' and output_format == 'html' and args.engine in ('markdown', 'markdown2'):
         body = markdown_filter(body, args.engine)
         body = html_filter(body, args.critic_template, args.critic_mode[0], args.standalone)
     elif output_format != args.from_format:
-        # defer standalone to pandoc
-        body = html_filter(body, args.critic_template, args.critic_mode[0], False)
-        body = pandoc_filter(body, args.from_format, output_format, args.standalone, args.engine)
-    else:
+        # as long as the final format will be latex, don't add html_filter
+        if args.to != 'latex':
+            # defer standalone to pandoc
+            body = html_filter(body, args.critic_template, args.critic_mode[0], False)
+        if is_binary:
+            # only pypandoc handles binary output
+            body = pandoc_filter(body, args.from_format, output_format, args.standalone, 'pypandoc', outputfile=args.output)
+        else:
+            body = pandoc_filter(body, args.from_format, output_format, args.standalone, args.engine)
+    elif args.to != 'latex':
         body = html_filter(body, args.critic_template, args.critic_mode[0], args.standalone)
 
-    args.output.write(body)
+    # write (if is binary, already written above)
+    if not is_binary:
+        args.output.write(body)
 
 
 def get_args():
-
     parser = argparse.ArgumentParser(description='Convert Critic Markup.')
 
     parser.add_argument('input', type=argparse.FileType('r'), default=sys.stdin,
                         help='Input file. Default: stdin.')
-    # TODO: handle binary output through pypandoc: ("odt", "docx", "epub", "epub3", "pdf")
-    parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=sys.stdout,
+    parser.add_argument('-o', '--output',
                         help='Output file. Default: stdout.')
 
     parser.add_argument('-t', '--to',
@@ -69,6 +73,9 @@ def get_args():
                         help='Input format. Default: inferred from --input.')
     parser.add_argument('-s', '--standalone', action='store_true',
                         help='Output standalone html.')
+
+    parser.add_argument('-i', '--inplace',
+                        help='Overwrite original file.')
 
     parser.add_argument('--critic-template', type=argparse.FileType('r'),
                         help='Custom template of CSS and JS for CriticMarkup in diff mode.')
@@ -85,12 +92,46 @@ def get_args():
 
     args = parser.parse_args()
 
-    if not args.to:
-        args.to = IO_to_format(args.output)
+    # from-format (args.input is a file descriptor)
     if not args.from_format:
-        args.from_format = IO_to_format(args.input)
-    return args
+        if args.input.name != '<stdin>':
+            args.from_format = normalize_format(os.path.splitext(args.input.name)[1][1:])
+        else:
+            print("No input file extension nor from-format specified. Default to markdown.")
+            args.from_format = 'markdown'
+
+    body = args.input.read()
+
+    if args.inplace:
+        if args.input.name == '<stdin>':
+            print('Cannot perform inplace to stdin!', file=sys.stderr)
+            exit(1)
+        else:
+            args.output = args.input.name
+
+    args.input.close()
+    
+
+    # output-format (remember args.output is a string)
+    try:
+        output_format = normalize_format(os.path.splitext(args.output)[1][1:])
+    except TypeError:
+        print("No output file extension nor to-format specified. Default to HTML.")
+        output_format = 'html'
+
+    if args.to is None:
+        args.to = output_format
+
+    is_binary = output_format in ("odt", "docx", "epub", "epub3", "pdf")
+
+    if not is_binary:
+        args.output = sys.stdout if args.output is None else open(args.output, 'w')
+    elif args.output is None:
+        print('Cannot output binary format to stdout', file=sys.stderr)
+        exit(1)
+
+    return args, body, output_format, is_binary
 
 
 def cli():
-    main(get_args())
+    main(*get_args())
